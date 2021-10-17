905029f159a9b758ce241895ec980bb7e0fb70b9
Jessica Lord <jess.lord@gmail.com>
2021-10-17 17:45:32 +0100

    Add RSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css/")
     eleventyConfig.addWatchTarget("./src/css/")
+    eleventyConfig.addPlugin(pluginRss)
     return {
         dir: {
             input: "src",
